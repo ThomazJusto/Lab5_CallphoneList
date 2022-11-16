cf26dd48bfd952d2e45d2fbb52dc0e1b2e07033a
ThomazJusto <thomaz.ritter207@gmail.com>
2022-11-16 10:37:53 -0300

    adding test structure with JUnit

---
+++ src/Tests/PersonContactTest.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import UI.ContactsUI;
+
+class PersonContactTest {
+	
+	@Test
+	public void test1() {
+		//
+		
+	}
+
+	@Test
+	public void test2() {
+		//
+		
+	}
+}
